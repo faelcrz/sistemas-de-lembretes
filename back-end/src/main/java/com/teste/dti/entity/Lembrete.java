eecd2eff3ef116e94658eb2863196d94e7d27f36
Rafael Cruz <rafaelcruzsanto@hotmail.com>
2023-05-10 11:37:04 -0300

    feat: back-end(Criação Entity Lembrete)

--- back-end/src/main/java/com/teste/dti/entity/Lembrete.java
+++ back-end/src/main/java/com/teste/dti/entity/Lembrete.java
@@ -1,5 +1,57 @@
 package com.teste.dti.entity;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Lembrete {
 
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private long id;
+	
+	@NotNull
+	private String nome;
+	
+	@Future
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private Date data;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+		
 }
